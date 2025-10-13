332e5e37303da72cc36f80c3001fcf80418487bf
Ricardo Prestes <ricardo.logan@hotmail.com>
2025-10-13 20:36:56 -0300

    Create project

---
+++ MyMovie/MyMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovie
+//
+//  Created by Ricardo Prestes on 13/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
